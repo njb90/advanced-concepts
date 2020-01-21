5934e201ed2d67d3e41cf3719ab897a6815ccf58
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-01-21 15:12:58 -0800

    cleanup

--- autoupload-fetch/auto-upload-private.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const cloudinary = require('cloudinary').v2;
-
-// https://cloudinary.com/blog/how_to_automatically_migrate_all_your_images_to_the_cloud
-
-// upload a spreadsheet
-console.log(cloudinary.url("autoupload-test/raw/test-spreadsheet.xlsx", 
-               { resource_type: 'raw',secure:true } ));
-
